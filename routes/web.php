dcd51f4992e7bc9b5b0e299fc866f32b7f6d9905
Sumon Mia <sumonahamed143.sa@gmail.com>
2019-04-09 22:08:56 +0600

    Created controller, Resource, products table, Routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route for Product
+Route::resource('product', 'ProductController');
